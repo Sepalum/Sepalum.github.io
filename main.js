95ce2830ff4df1bf8a20c59b590499cf793bb42f
Sepalum <sepalum1@gmail.com>
2020-10-05 00:09:21 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const data = '[[{"day":29,"schedule":[],"isMonth":false},{"day":30,"schedule":[],"isMonth":false},{"day":31,"schedule":[],"isMonth":false},{"day":1,"schedule":[],"isMonth":true},{"day":2,"schedule":[],"isMonth":true},{"day":3,"schedule":[],"isMonth":true},{"day":4,"schedule":[],"isMonth":true},{"day":5,"schedule":[],"isMonth":true},{"day":6,"schedule":[],"isMonth":true},{"day":7,"schedule":[],"isMonth":true},{"day":8,"schedule":[],"isMonth":true},{"day":9,"schedule":[],"isMonth":true},{"day":10,"schedule":[],"isMonth":true},{"day":11,"schedule":[],"isMonth":true},{"day":12,"schedule":[],"isMonth":true},{"day":13,"schedule":[],"isMonth":true},{"day":14,"schedule":[],"isMonth":true},{"day":15,"schedule":[],"isMonth":true},{"day":16,"schedule":[],"isMonth":true},{"day":17,"schedule":[],"isMonth":true},{"day":18,"schedule":[],"isMonth":true},{"day":19,"schedule":[],"isMonth":true},{"day":20,"schedule":[],"isMonth":true},{"day":21,"schedule":[],"isMonth":true},{"day":22,"schedule":[],"isMonth":true},{"day":23,"schedule":[],"isMonth":true},{"day":24,"schedule":[],"isMonth":true},{"day":25,"schedule":[],"isMonth":true},{"day":26,"schedule":[],"isMonth":true},{"day":27,"schedule":[],"isMonth":true},{"day":28,"schedule":[],"isMonth":true},{"day":29,"schedule":[],"isMonth":true},{"day":30,"schedule":[],"isMonth":true},{"day":31,"schedule":[],"isMonth":true},{"day":1,"schedule":[],"isMonth":false},{"day":2,"schedule":[],"isMonth":false},{"day":3,"schedule":[],"isMonth":false},{"day":4,"schedule":[],"isMonth":false},{"day":5,"schedule":[],"isMonth":false},{"day":6,"schedule":[],"isMonth":false},{"day":7,"schedule":[],"isMonth":false},{"day":8,"schedule":[],"isMonth":false}],[{"day":26,"schedule":[],"isMonth":false},{"day":27,"schedule":[],"isMonth":false},{"day":28,"schedule":[],"isMonth":false},{"day":29,"schedule":[],"isMonth":false},{"day":30,"schedule":[],"isMonth":false},{"day":31,"schedule":[],"isMonth":false},{"day":1,"schedule":[],"isMonth":true},{"day":2,"schedule":[],"isMonth":true},{"day":3,"schedule":[],"isMonth":true},{"day":4,"schedule":[],"isMonth":true},{"day":5,"schedule":[],"isMonth":true},{"day":6,"schedule":[],"isMonth":true},{"day":7,"schedule":[],"isMonth":true},{"day":8,"schedule":[],"isMonth":true},{"day":9,"schedule":[],"isMonth":true},{"day":10,"schedule":[],"isMonth":true},{"day":11,"schedule":[],"isMonth":true},{"day":12,"schedule":[],"isMonth":true},{"day":13,"schedule":[],"isMonth":true},{"day":14,"schedule":[],"isMonth":true},{"day":15,"schedule":[],"isMonth":true},{"day":16,"schedule":[],"isMonth":true},{"day":17,"schedule":[],"isMonth":true},{"day":18,"schedule":[],"isMonth":true},{"day":19,"schedule":[],"isMonth":true},{"day":20,"schedule":[],"isMonth":true},{"day":21,"schedule":[],"isMonth":true},{"day":22,"schedule":[],"isMonth":true},{"day":23,"schedule":[],"isMonth":true},{"day":24,"schedule":[],"isMonth":true},{"day":25,"schedule":[],"isMonth":true},{"day":26,"schedule":[],"isMonth":true},{"day":27,"schedule":[],"isMonth":true},{"day":28,"schedule":[],"isMonth":true},{"day":29,"schedule":[],"isMonth":true},{"day":1,"schedule":[],"isMonth":false},{"day":2,"schedule":[],"isMonth":false},{"day":3,"schedule":[],"isMonth":false},{"day":4,"schedule":[],"isMonth":false},{"day":5,"schedule":[],"isMonth":false},{"day":6,"schedule":[],"isMonth":false},{"day":7,"schedule":[],"isMonth":false}],[{"day":1,"schedule":[],"isMonth":true},{"day":2,"schedule":[],"isMonth":true},{"day":3,"schedule":[],"isMonth":true},{"day":4,"schedule":[],"isMonth":true},{"day":5,"schedule":[],"isMonth":true},{"day":6,"schedule":[],"isMonth":true},{"day":7,"schedule":[],"isMonth":true},{"day":8,"schedule":[],"isMonth":true},{"day":9,"schedule":[],"isMonth":true},{"day":10,"schedule":[],"isMonth":true},{"day":11,"schedule":[],"isMonth":true},{"day":12,"schedule":[],"isMonth":true},{"day":13,"schedule":[],"isMonth":true},{"day":14,"schedule":[],"isMonth":true},{"day":15,"schedule":[],"isMonth":true},{"day":16,"schedule":[],"isMonth":true},{"day":17,"schedule":[],"isMonth":true},{"day":18,"schedule":[],"isMonth":true},{"day":19,"schedule":[],"isMonth":true},{"day":20,"schedule":[],"isMonth":true},{"day":21,"schedule":[],"isMonth":true},{"day":22,"schedule":[],"isMonth":true},{"day":23,"schedule":[],"isMonth":true},{"day":24,"schedule":[],"isMonth":true},{"day":25,"schedule":[],"isMonth":true},{"day":26,"schedule":[],"isMonth":true},{"day":27,"schedule":[],"isMonth":true},{"day":28,"schedule":[],"isMonth":true},{"day":29,"schedule":[],"isMonth":true},{"day":30,"schedule":[],"isMonth":true},{"day":31,"schedule":[],"isMonth":true},{"day":1,"schedule":[],"isMonth":false},{"day":2,"schedule":[],"isMonth":false},{"day":3,"schedule":[],"isMonth":false},{"day":4,"schedule":[],"isMonth":false},{"day":5,"schedule":[],"isMonth":false},{"day":6,"schedule":[],"isMonth":false},{"day":7,"schedule":[],"isMonth":false},{"day":8,"schedule":[],"isMonth":false},{"day":9,"schedule":[],"isMonth":false},{"day":10,"schedule":[],"isMonth":false},{"day":11,"schedule":[],"isMonth":false}],[],[],[],[],[],[{"day":30,"schedule":[],"isMonth":false},{"day":31,"schedule":[],"isMonth":false},{"day":1,"schedule":[],"isMonth":true},{"day":2,"schedule":[],"isMonth":true},{"day":3,"schedule":[],"isMonth":true},{"day":4,"schedule":[],"isMonth":true},{"day":5,"schedule":[],"isMonth":true},{"day":6,"schedule":[],"isMonth":true},{"day":7,"schedule":[],"isMonth":true},{"day":8,"schedule":[],"isMonth":true},{"day":9,"schedule":[],"isMonth":true},{"day":10,"schedule":[],"isMonth":true},{"day":11,"schedule":[],"isMonth":true},{"day":12,"schedule":[],"isMonth":true},{"day":13,"schedule":[],"isMonth":true},{"day":14,"schedule":[],"isMonth":true},{"day":15,"schedule":[],"isMonth":true},{"day":16,"schedule":[],"isMonth":true},{"day":17,"schedule":[],"isMonth":true},{"day":18,"schedule":[],"isMonth":true},{"day":19,"schedule":[],"isMonth":true},{"day":20,"schedule":[],"isMonth":true},{"day":21,"schedule":[],"isMonth":true},{"day":22,"schedule":[],"isMonth":true},{"day":23,"schedule":[],"isMonth":true},{"day":24,"schedule":[],"isMonth":true},{"day":25,"schedule":[],"isMonth":true},{"day":26,"schedule":[],"isMonth":true},{"day":27,"schedule":[],"isMonth":true},{"day":28,"schedule":[],"isMonth":true},{"day":29,"schedule":[],"isMonth":true},{"day":30,"schedule":[],"isMonth":true},{"day":1,"schedule":[],"isMonth":false},{"day":2,"schedule":[],"isMonth":false},{"day":2,"schedule":[],"isMonth":false},{"day":3,"schedule":[],"isMonth":false},{"day":4,"schedule":[],"isMonth":false},{"day":5,"schedule":[],"isMonth":false},{"day":6,"schedule":[],"isMonth":false},{"day":7,"schedule":[],"isMonth":false},{"day":8,"schedule":[],"isMonth":false},{"day":9,"schedule":[],"isMonth":false}],[{"day":27,"schedule":[],"isMonth":false},{"day":28,"schedule":[],"isMonth":false},{"day":29,"schedule":[],"isMonth":false},{"day":30,"schedule":[],"isMonth":false},{"day":1,"schedule":[],"isMonth":true},{"day":2,"schedule":[],"isMonth":true},{"day":3,"schedule":[],"isMonth":true},{"day":4,"schedule":[],"isMonth":true},{"day":5,"schedule":[],"isMonth":true},{"day":6,"schedule":[],"isMonth":true},{"day":7,"schedule":[],"isMonth":true},{"day":8,"schedule":[],"isMonth":true},{"day":9,"schedule":[],"isMonth":true},{"day":10,"schedule":[],"isMonth":true},{"day":11,"schedule":[],"isMonth":true},{"day":12,"schedule":["과학탐구실험","영수증 탐구 제출","진로","탐구 제출"],"isMonth":true},{"day":13,"schedule":[],"isMonth":true},{"day":14,"schedule":[],"isMonth":true},{"day":15,"schedule":[],"isMonth":true},{"day":16,"schedule":[],"isMonth":true},{"day":17,"schedule":[],"isMonth":true},{"day":18,"schedule":[],"isMonth":true},{"day":19,"schedule":[],"isMonth":true},{"day":20,"schedule":[],"isMonth":true},{"day":21,"schedule":[],"isMonth":true},{"day":22,"schedule":[],"isMonth":true},{"day":23,"schedule":[],"isMonth":true},{"day":24,"schedule":[],"isMonth":true},{"day":25,"schedule":[],"isMonth":true},{"day":26,"schedule":[],"isMonth":true},{"day":27,"schedule":[],"isMonth":true},{"day":28,"schedule":[],"isMonth":true},{"day":29,"schedule":[],"isMonth":true},{"day":30,"schedule":["국어","사실없음","수학","업승ㅁ"],"isMonth":true},{"day":31,"schedule":[],"isMonth":true},{"day":1,"schedule":[],"isMonth":false},{"day":2,"schedule":[],"isMonth":false},{"day":3,"schedule":[],"isMonth":false},{"day":4,"schedule":[],"isMonth":false},{"day":5,"schedule":[],"isMonth":false},{"day":6,"schedule":[],"isMonth":false},{"day":7,"schedule":[],"isMonth":false}],[],[]]'
+const myData = JSON.parse(data)
+let now = new Date();
+let nowMonth = now.getMonth();
+window.onload = function() {
+    document.forms.form.monthSelect.value = nowMonth+1 + "월"
+    for (let i = 0; i < 42; i++) {
+        if (myData[nowMonth][i].schedule.length != 0) {
+            document.getElementById(i + 1).classList.add("bold")
+        }
+        document.getElementById(i + 1).innerText = String(myData[nowMonth][i].day)
+    }
+    if (document.getElementById("36").innerText < 10) {
+        document.getElementById("myTable").deleteRow(6);
+    }
+}
+function changeMonth() {
+    let month = document.forms.form.monthSelect.value.slice(0, -1) - 1;
+    for (let i = 0; i < 42; i++) {
+        if (myData[month][i].schedule.length != 0) {
+            document.getElementById(i + 1).classList.add("bold");
+        }
+        else {
+            document.getElementById(i + 1).classList.remove("bold")
+        }
+        document.getElementById(i + 1).innerText = String(myData[month][i].day)
+    }
+    if (document.getElementById("36").innerText < 10) {
+        document.getElementById("myTable").deleteRow(6);
+    }
+    if (document.getElementById("35").innerText > 20) {
+        document.getElementById("myTable").insertRow(7);
+    }
+}
+
+function clickEvent(tr) {
+    let month = document.forms.form.monthSelect.value.slice(0, -1) - 1;
+    if (myData[month][tr.id-1].schedule.length != 0) {
+        let texts = []
+        for (let n = 0; 2*n <= myData[month][tr.id-1].schedule.length; n+=2) {
+            texts[n] = `${myData[month][tr.id-1].schedule[n]} : ${myData[month][tr.id-1].schedule[n+1]}`
+        }
+        texts = texts.filter(item => item);
+        document.getElementById("textBox").innerText = texts.join("\n")
+    }
+    else {
+        document.getElementById("textBox").innerText = "오늘은 수행평가가 없는 날입니다."
+    }
+}
+if (!clickEvent()) {
+    document.getElementById("textBox").innerText = "날짜를 선택해주세요"
+}
